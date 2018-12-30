5e8164c8404ec6bc844986592f8b00ca514b6fdb
Calvin Dong <38579516+cdong5@users.noreply.github.com>
2018-12-30 02:23:00 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,22 +1,62 @@
+# Created by Calvin Dong - 12/30/2018
+# Learning tkinter and random Library
+
 from random import *
 from tkinter import *
 
 def exercise():
+    # Reads a txt file named exercises
+    # Places each line of text into a list
+    # Uses the list to generate exercises
     exerciselist = []
     file = open('exercises.txt', 'r')
     for line in file:
         ex = line.strip()
         exerciselist.append(ex)
-    ran_ex = exerciselist[randint(0,len(exerciselist))]
+    len_exlist = int(len(exerciselist))
+    ran_ex = exerciselist[randint(0,len_exlist-1)]
     return ran_ex
 
-def gen():
+def num_sec_ex():
+    # Creates a random number of reps to do for exercise
+    ex_time = randint(10, 20)
+    return ex_time
+
+def gen_ex():
+    # Creates a Label of the exercise and number of reps
+    # Places into the application
     textframe = Frame(root)
     textframe.pack()
-    time = randint(5,15)
-    e1 = Label(textframe, text=(f'{exercise()} for {time} reps'))
+    e1 = Label(textframe, text=(f'{exercise()} for {num_sec_ex()} reps'))
     e1.pack()
 
+def num_sec_walk():
+    # Creates a random variable to tell how long to walk
+    walk_time = randint(120,240)
+    return walk_time
+
+def gen_walking():
+    # Generates a label to tell how long to walk for
+    # Places into the application
+    textframe = Frame(root)
+    textframe.pack()
+    e2 = Label(textframe, text=(f'Walk for {round(num_sec_walk()//60, 2)} minutes'))
+    e2.pack()
+    return e2
+
+def create_list():
+    # Creates a random list of exercises and walking time
+    # Gets the time you have, and creates a list of exercises to fit in the time
+    time = entry_box.get()
+    current_time = 0
+    total_time = int(time)*60
+    while current_time < total_time:
+        gen_walking()
+        gen_ex()
+        current_time += num_sec_ex()
+        current_time += num_sec_walk()
+
+# Formating and organization of the GUI
 root = Tk()
 root.title('Dog Walk Exerciser')
 
@@ -36,7 +76,7 @@ entry_box.pack(side="left", padx=2, pady=2)
 gen_frame = Frame(root)
 gen_frame.pack(side=TOP)
 
-gen_button = Button(gen_frame, text='Generate', command=gen)
+gen_button = Button(gen_frame, text='Generate', command= create_list)
 gen_button.pack(pady=2)
 
 root.mainloop()
